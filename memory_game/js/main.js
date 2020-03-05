4cc5c0be92e1728712e9d19664445211bbb5aaca
connor richter <connorrichter@connors-mbp.attlocal.net>
2020-03-05 09:19:14 -0600

    improved game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 // creating variables for each card
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 const cardsInPlay = [];
 
 function checkMatch(){
@@ -13,8 +34,10 @@ function checkMatch(){
 }
 
 function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId])
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
     checkMatch();
 }
 
